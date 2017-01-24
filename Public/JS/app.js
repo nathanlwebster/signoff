20ed0bd1357c3b2a093c489857414b0aa1fd3197
Nathan Webster <nathanlwebster@gmail.com>
2017-01-24 10:13:14 -0700

    Add report instructions, other data categories

--- Public/JS/app.js
+++ Public/JS/app.js
@@ -3,20 +3,20 @@
 $(function(){
 	
     var model = {
+        reportTitle:null,
         numColumns:null,
         numRows:null,
         minRow:null,
         minCol:null,
         maxRow:null,
         maxCol:null,
-        rowHeaders: [],
+        colHeaders: [],
         colOptions: [],
         init: function() {
             this.minRow = 1;
             this.minCol = 1;
             this.maxRow = 5;
             this.maxCol = null;
-
         }        
     };
 
@@ -27,7 +27,10 @@ $(function(){
 
 	var controller = {
         buildTable: function() {
+            model.reportTitle = "My New Report"; //input from user
             model.numColumns = 5; //input from user
+            model.numRows = 10; //input from column 1 selection
+            model.colHeaders = ["Column 1", "Column 2", "Column 3", "Column 4", "Column 5"]; //from create report by options
             
         }
     };
