af346a2fb4de04ad59903448080a3a719df520b8
Nathan Webster <nathanlwebster@gmail.com>
2017-02-10 12:17:35 -0700

    Add angular-ui-bootstrap

--- Public/JS/angular_app.js
+++ Public/JS/angular_app.js
@@ -1,4 +1,4 @@
-var app = angular.module('reportApp', ['ngStorage', 'ngRoute']);
+var app = angular.module('reportApp', ['ngStorage', 'ngRoute', 'ui-bootstrap']);
 
 
 app.config(function($routeProvider, $locationProvider) {
